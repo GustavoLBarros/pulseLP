653f8eb286f8199f618fbba20c8001c3ceada477
Iago Lusvordes <iagolusvordes@gmail.com>
2026-04-09 15:27:46 -0300

    corrigido whatsapp + adicionado analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Script from 'next/script';
 
 export const metadata = {
   title: 'Pulse',
@@ -15,7 +16,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-3B7KRMX8D6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3B7KRMX8D6');
+          `}
+        </Script>
+
+        {children}
+      </body>
     </html>
   );
 }
